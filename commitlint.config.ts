26b0c6cabe925e4a3d7d4d5c6953c4d1e5b62b39
liminfinity <108136720+liminfinity@users.noreply.github.com>
2025-01-22 00:03:17 +0700

    build(format): setup prettier and editorconfig
    META: OBR-66

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,5 +1,8 @@
-import type {UserConfig} from '@commitlint/types'
+import type { UserConfig } from '@commitlint/types';
 
-const config: UserConfig = { extends: ['@commitlint/config-conventional'], formatter: '@commitlint/format' }
+const config: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  formatter: '@commitlint/format',
+};
 
 export default config;
