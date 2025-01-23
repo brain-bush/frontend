0599434dee931e7e4a32f71dfd04e9c03479dcc7
liminfinity <108136720+liminfinity@users.noreply.github.com>
2025-01-23 21:15:53 +0700

    build(redux): configure redux for nextjs and add provider for it
    META: OBR-94

---
+++ src/global.d.ts
@@ -0,0 +1,28 @@
+import type * as AppTypes from '@/app/types';
+
+declare global {
+  /**
+   * ⚠️ FSD
+   *
+   * Its hack way to export redux inferring types from @/app/types
+   * and use it in @/shared/hooks
+   */
+  declare type AppStore = AppTypes.AppStore;
+
+  /**
+   * ⚠️ FSD
+   *
+   * Its hack way to export redux inferring types from @/app/types
+   * and use it in @/shared/hooks
+   */
+  declare type RootState = AppTypes.RootState;
+
+  /**
+   * ⚠️ FSD
+   *
+   * Its hack way to export redux inferring types from @/app/types
+   * and use it in @/shared/hooks
+   */
+  declare type AppDispatch = AppTypes.AppDispatch;
+}
+export {};
