210b453097d4c96580e19ebd0aa20f4e2883ca31
liminfinity <108136720+liminfinity@users.noreply.github.com>
2025-01-23 21:50:19 +0700

    feat(auth): add base query for api and add auth environment
    META: OBR-31

---
+++ environment.d.ts
@@ -0,0 +1,7 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_SERVER_URL: string;
+    NEXT_PUBLIC_YA_OAUTH_CLIENT_ID: string;
+    NEXT_PUBLIC_YA_OAUTH_URL: string;
+  }
+}
--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,1 +1,3 @@
 export type { DefaultProps } from './defaultProps';
+export type { JoinStrings } from './joinStrings';
+export type { DefaultState } from './defaultState';
