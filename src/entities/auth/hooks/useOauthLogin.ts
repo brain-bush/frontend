97b33e0604385e4fce1731620e58a3fbbb9dfb2d
liminfinity <108136720+liminfinity@users.noreply.github.com>
2025-01-28 20:53:24 +0700

    feat(auth): add client id and edit login request field for token
    META: OBR-101

--- src/entities/auth/hooks/useOauthLogin.ts
+++ src/entities/auth/hooks/useOauthLogin.ts
@@ -8,15 +8,15 @@ import { useAppDispatch } from '@/shared/model';
  * Custom hook to handle OAuth login using a token.
  * Dispatches a login action with the provided token when the token is available.
  *
- * @param token - The OAuth token to be used for login. If null, no action is taken.
+ * @param oauthToken - The OAuth token to be used for login. If null, no action is taken.
  */
 
-export const useOauthLogin = (token: string | null): void => {
+export const useOauthLogin = (oauthToken: string | null): void => {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    if (token) {
-      dispatch(actions.login({ token }));
+    if (oauthToken) {
+      dispatch(actions.login({ oauthToken }));
     }
-  }, [token, dispatch]);
+  }, [oauthToken, dispatch]);
 };
--- src/entities/auth/hooks/useYaOauthToken.ts
+++ src/entities/auth/hooks/useYaOauthToken.ts
@@ -1,18 +1,23 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation';
-
 /**
- * Hook to get the OAuth token from the URL search parameters.
+ * Returns the OAuth token from the URL hash if it's present.
  *
- * @returns The OAuth token if it is present in the URL search parameters, or null if it is not.
+ * On success, returns the OAuth token as a string. On failure, returns null.
+ *
+ * Example:
+ *   Given the URL `http://localhost:3000#access_token=token123`
+ *   `useYaOauthToken()` will return `token123`.
+ *
+ * @returns {string | null} The OAuth token or null.
  */
 export const useYaOauthToken = (): string | null => {
-  const searchParams = useSearchParams();
-
-  if (!searchParams) return null;
-
-  const token = searchParams.get('access_token');
-
-  return token;
+  const hash = window.location.hash;
+  if (hash) {
+    const queryParams = new URLSearchParams(hash.slice(1));
+    const token = queryParams.get('access_token');
+    return token;
+  } else {
+    return null;
+  }
 };
